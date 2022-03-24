e38d4514b4b4123f222969ae84a380f65dc4849b
ccarlosuarezz <karlosuarez710@gmail.com>
2022-03-23 21:34:28 -0500

    Scraping mercadolibre y linio

--- Product.py
+++ Product.py
@@ -6,16 +6,28 @@ class Product:
     def __init__(self, title, image, price, url):
         self.title = self.cleanText(str(title))
         self.image = image
-        self.price = price
+        self.price = self.cleanPrice(str(price))
         self.url = url
         self.description = ''
         
-    def setDescription(self, classDescription):
+    def setDescription(self, tag, classDescription, app):
         productRequest = requests.get(self.url)
         productSoup = BeautifulSoup(productRequest.content, 'html.parser')
-        newDescription = productSoup.find_all('p', attrs={"class":classDescription})
+        if app == 'linio':
+            newDescription = productSoup.find_all('div', attrs={"id":"panel-features"})
+            productSoup = BeautifulSoup(str(newDescription), 'html.parser')
+            newDescription = productSoup.find_all(tag, attrs={"class":classDescription})
+            productSoup = BeautifulSoup(str(newDescription), 'html.parser')
+            newDescription = productSoup.find_all('li')
+            newDescription = [i.text for i in newDescription]
+        else:
+            newDescription = productSoup.find_all(tag, attrs={"class":classDescription})
         self.description = self.cleanText(str(newDescription))
     
     def cleanText(self, text):
-        newText = text.replace(',', '.').strip()
+        newText = text.replace(',', '.').replace('"', '').strip()
+        return newText
+    
+    def cleanPrice(self, text):
+        newText = text.replace(' ', '').replace('$', '').replace('\n', '').replace('\t', '').replace('\r', '').strip()
         return newText
--- scrapingWithBs4.py
+++ scrapingWithBs4.py
@@ -17,7 +17,7 @@ productList = []
 def scrapingMercadoLibre():
     for search in searchList:
         # Request de la pagina
-        r = requests.get('https://deportes.mercadolibre.com.co/'+searchList[0]+'/')
+        r = requests.get('https://deportes.mercadolibre.com.co/'+search+'/')
 
         # imprimir status del request
         # print(r.status_code)
@@ -43,69 +43,38 @@ def scrapingMercadoLibre():
 
         for i in range(len(titleItems)):
             newProduct = Product(titleItems[i], imageItems[i], priceItems[i], urlItems[i])
-            newProduct.setDescription('ui-pdp-description__content')
+            newProduct.setDescription('p', 'ui-pdp-description__content', '')
             productList.append(newProduct)
 
 
 def scrapingLinio():
-    r = requests.get('https://www.linio.com.co/search?scroll=&q='+searchList[0])
-    soup = BeautifulSoup(r.content, 'html.parser')
-    titleItems = soup.find_all('span', attrs={"class":"title-section"})
-    titleItems = [i.text for i in titleItems]
-    dom = etree.HTML(str(soup))
-    imageItems = dom.xpath('//div[@class="image-container"]//img[@class="image"]')
-    imageItems = [i.get('data-src') for i in imageItems]
-    urlItems = soup.find_all('a', attrs={"class":"col-12 pl-0 pr-0"})
-    urlItems = ['https://www.linio.com.co/'+i.get('href') for i in urlItems]
-    priceItems = soup.find_all('span', attrs={"class":"price-main-md"})
-    priceItems = [i.text for i in priceItems]
-    for i in range(len(titleItems)):
-        productList.append(Product(titleItems[i], imageItems[i], priceItems[i], urlItems[i]))
-
-def scrapingDeportesregol():
-    r = requests.get('https://deportesregol.com/tienda/seccion/inicio/deportes/'+searchList[0]+'/')
-    soup = BeautifulSoup(r.content, 'html.parser')
-    titleItems = soup.find_all('h2', attrs={"class":"ui-search-item__title"})
-    titleItems = [i.text for i in titleItems]
-    imageItems = soup.find_all('img', attrs={"class":"ui-search-result-image__element"})
-    imageItems = [i.get('data-src') for i in imageItems]
-    urlItems = soup.find_all('a', attrs={"class":"ui-search-item__group__element ui-search-link"})
-    urlItems = [i.get('href') for i in urlItems]
-    dom = etree.HTML(str(soup))
-    priceItems = dom.xpath('//li[@class="ui-search-layout__item"]//div[@class="ui-search-item__group ui-search-item__group--price"]//div[@class="ui-search-price ui-search-price--size-medium"]//span[@class="price-tag ui-search-price__part"]//span[@class="price-tag-fraction"]')
-    priceItems = [i.text for i in priceItems]
-    for i in range(len(titleItems)):
-        productList.append(Product(titleItems[i], imageItems[i], priceItems[i], urlItems[i]))
-
-
-def scrapingDecathlon():
-    # https://www.decathlon.com.co/?prod_co%5Bquery%5D=ciclismo
-    r = requests.get('https://www.decathlon.com.co/?prod_co%5Bquery%5D='+searchList[0])
-    soup = BeautifulSoup(r.content, 'html.parser')
-    titleItems = soup.find_all('h2', attrs={"class":"ui-search-item__title"})
-    titleItems = [i.text for i in titleItems]
-    imageItems = soup.find_all('img', attrs={"class":"ui-search-result-image__element"})
-    imageItems = [i.get('data-src') for i in imageItems]
-    urlItems = soup.find_all('a', attrs={"class":"ui-search-item__group__element ui-search-link"})
-    urlItems = [i.get('href') for i in urlItems]
-    dom = etree.HTML(str(soup))
-    priceItems = dom.xpath('//li[@class="ui-search-layout__item"]//div[@class="ui-search-item__group ui-search-item__group--price"]//div[@class="ui-search-price ui-search-price--size-medium"]//span[@class="price-tag ui-search-price__part"]//span[@class="price-tag-fraction"]')
-    priceItems = [i.text for i in priceItems]
-    for i in range(len(titleItems)):
-        productList.append(Product(titleItems[i], imageItems[i], priceItems[i], urlItems[i]))
+    for search in searchList:
+        r = requests.get('https://www.linio.com.co/search?scroll=&q='+search)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        dom = etree.HTML(str(soup))
+        titleItems = dom.xpath('//div[@class="image-container"]//img[@class="image"]')
+        titleItems = [i.get('alt') for i in titleItems]
+        imageItems = dom.xpath('//div[@class="image-container"]//img[@class="image"]')
+        imageItems = ['https:'+i.get('data-lazy') for i in imageItems]
+        urlItems = soup.find_all('a', attrs={"class":"col-12 pl-0 pr-0"})
+        urlItems = ['https://www.linio.com.co'+i.get('href') for i in urlItems]
+        priceItems = soup.find_all('span', attrs={"class":"price-main-md"})
+        priceItems = [i.text for i in priceItems]
+        for i in range(len(titleItems)):
+            newProduct = Product(titleItems[i], imageItems[i], priceItems[i], urlItems[i])
+            newProduct.setDescription('div', 'product-bg-container', 'linio')
+            productList.append(newProduct)
 
 def generateCSV():
     scrapingMercadoLibre()
-    # scrapingLinio()
-    # scrapingDeportesregol()
-    # scrapingDecathlon()
+    scrapingLinio()
     dataFrameProducts = pd.DataFrame(columns = ['Title', 'Image', 'Price', 'URL', 'Description'])
     for product in productList:
         new_row = pd.DataFrame.from_records([{
             'Title': product.title,
-            'Image': product.image+' ',
+            'Image': product.image,
             'Price': product.price,
-            'URL': product.url+' ',
+            'URL': product.url,
             'Description': product.description
         }])
         dataFrameProducts = pd.concat([dataFrameProducts, new_row])
